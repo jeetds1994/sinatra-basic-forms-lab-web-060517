90d94360e7132c2f58c8064b06db53811cc09d47
Diljeet <jeetds1994@gmail.com>
2017-06-28 13:11:42 -0400

    halfway done

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  set :views, 'views'
+  get '/' do
+    erb :index
+  end
+
+  get '/create-puppy' do
+    erb :create_puppy
+  end
+
+  post '/display-puppy' do
+    erb :display_puppy
+  end
 
 end
